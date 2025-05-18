f57a7b211f30c2047f8b3b7facac3aed543ad212
Soumili Ghosh <118499069+SG1947@users.noreply.github.com>
2025-05-19 07:58:54 +0800

    Add files via upload

---
+++ arithmetic_logical_ops.py
@@ -0,0 +1,5 @@
+# Arithmetic and logical operator examples
+print("Addition:", 5 + 3)
+print("Division:", 10 / 2)
+print("Modulus:", 7 % 2)
+print("Logical AND:", 3 > 1 and 4 < 6)
---
+++ rectangle.py
@@ -0,0 +1,10 @@
+# Calculate area and perimeter of a rectangle
+length = 5
+width = 3
+
+area = length * width
+perimeter = 2 * (length + width)
+
+print("Area:", area)
+print("Perimeter:", perimeter)
+
